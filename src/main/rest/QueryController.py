d0282293127a6f56720f25804ba1e559dbffc1a2
Sean Nicholson <sean.nicholson.18@ucl.ac.uk>
2019-04-17 23:58:03 +0100

    email client

--- src/main/rest/QueryController.py
+++ src/main/rest/QueryController.py
@@ -1,5 +1,8 @@
 import json
+
+import pandas
 from flask_classy import FlaskView, route
+import pandas as pd
 
 from src.main.rest.QueryManager import QueryManager
 from src.main.rest.SessionManager import SessionManager, AuthenticationError
@@ -25,6 +28,20 @@ class QueryController(FlaskView):
 
         measurements = self.queryManager.getMeasurement(session)
         return json.dumps(measurements)
+
+    @route("/getDataframe/<string:auth>")
+    def getDataframe(self, auth):
+        split = auth.split(":")
+        name = split[0]
+        sessionID = split[1]
+        try:
+            session = self.sessionManager.getSession(sessionID, name)
+        except AuthenticationError as e:
+            return e
+        measurements = self.queryManager.getMeasurement(session)
+        df = pd.DataFrame(measurements["measurement"])
+        fileLocation = 'temp/{}.csv'.format(name)
+        df.to_csv(fileLocation)
 
 
 # <src.main.domain.Session.Session object at 0x105c25b00>
